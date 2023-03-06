49643fbc1671f2d97f19c6a83200a2537da7c165
Aansh Basu <96300383+Aansh2003@users.noreply.github.com>
2023-03-06 11:24:38 +0530

    update constructor,print

--- numpp.hpp
+++ numpp.hpp
@@ -1,22 +1,48 @@
 #include <vector>
+#include <iostream>
 
 namespace numpp
 {
+	template<class T, int N> int size(T (&r)[N]){
+		return N;
+	}
+	
 	template <typename T> class Array{
-		
-		std::vector<T> value;
-		
-		Array(T input[])
+		public:
+			std::vector<T> value;
+			
+			Array(T input[],int len);
+			Array(T input);
+	};
+	
+	// Function definitions of class Array
+	template <typename T> Array<T>::Array(T input[],int len)
+	{	
+		for(int i=0;i<len;i++)
 		{
-			for(auto i:input)
+			value.push_back(input[i]);
+		}
+	}
+	
+	template <typename T> Array<T>::Array(T input)
+	{
+		value.push_back(input);
+	}
+	
+	
+	
+	template <typename T> static void print(Array<T> val)
+	{
+		try{
+			for(auto i:val.value)
 			{
-				value.push_back(i);
+				std::cout<<i<<" ";
 			}
 		}
-		
-		Array(T input)
+		catch(...)
 		{
-			value.push_back(input);
+			return;
 		}
-	};
+	}
+	
 }
