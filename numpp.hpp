99e3050b1794543e02c5b6339312c186593e008c
Aansh Basu <96300383+Aansh2003@users.noreply.github.com>
2023-03-07 09:09:17 +0530

    Structure change

--- numpp.hpp
+++ numpp.hpp
@@ -1,48 +1,4 @@
 #include <vector>
 #include <iostream>
 
-namespace numpp
-{
-	template<class T, int N> int size(T (&r)[N]){
-		return N;
-	}
-	
-	template <typename T> class Array{
-		public:
-			std::vector<T> value;
-			
-			Array(T input[],int len);
-			Array(T input);
-	};
-	
-	// Function definitions of class Array
-	template <typename T> Array<T>::Array(T input[],int len)
-	{	
-		for(int i=0;i<len;i++)
-		{
-			value.push_back(input[i]);
-		}
-	}
-	
-	template <typename T> Array<T>::Array(T input)
-	{
-		value.push_back(input);
-	}
-	
-	
-	
-	template <typename T> static void print(Array<T> val)
-	{
-		try{
-			for(auto i:val.value)
-			{
-				std::cout<<i<<" ";
-			}
-		}
-		catch(...)
-		{
-			return;
-		}
-	}
-	
-}
+#include "Array.hpp"
