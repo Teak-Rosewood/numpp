e7fe833b9188a816201cea5aea3d94fdc5ea36c3
Aansh Basu <96300383+Aansh2003@users.noreply.github.com>
2023-03-14 11:08:26 +0530

    prim_array

---
+++ prim_array.hpp
@@ -0,0 +1,43 @@
+#include "Array.hpp"
+
+#ifndef _PRIM_ARR_
+#define _PRIM_ARR_
+
+namespace numpp
+{
+	template<class T, int N> Array<T> toArray(T (&r)[N]);
+	template<class T, int N> int size(T (&r)[N]);
+	/**
+	* Allows conversion of arrays to Array object without information about size.
+	*
+	* PARAMS:
+	* 	 T (&r)[N] : input of array of type T(template)
+	* RETURN:
+	*	 Array<T> : numpp::Array object
+	*/
+	
+	template<class T, int N> Array<T> toArray(T (&r)[N])
+	{
+		Array<T> array;
+		for(int i=0;i<N;i++)
+		{
+			array.value.push_back(*(r+i));
+		}
+		return array;
+	}
+	
+	/**
+	* returns size of any array.
+	*
+	* PARAMS:
+	*	 T (&r)[N] : input of array of type T(template)
+	* RETURN:
+	*	 int : size of array
+	*/
+	template<class T, int N> int size(T (&r)[N])
+	{
+		return N;
+	}
+}
+
+#endif
